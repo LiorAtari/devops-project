b11e2be711df4b35b0fbc75a97bb0da3320a210d
Lior Atari <134922718+LiorAtari@users.noreply.github.com>
2025-03-31 10:29:51 +0300

    Adding enabling of APIs and creation of project

--- terraform/01-gke-cluster/main.tf
+++ terraform/01-gke-cluster/main.tf
@@ -11,3 +11,27 @@ provider "google" {
   project = var.project
   region  = var.region
 }
+
+resource "google_project" "develeap-task" {
+  name       = var.project
+  project_id = var.project
+}
+
+# Enabling APIs required which are not enabled by default
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+  project = var.project
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "kubernetes" {
+  service = "container.googleapis.com"
+  project = var.project
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "secret_manager" {
+  service = "secretmanager.googleapis.com"
+  project = var.project
+  disable_on_destroy = false
+}
